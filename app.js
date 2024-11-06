ec422329141827f9f18ae919183e47e13cf7628d
benjamingohps <benjamingohps@gmail.com>
2024-11-07 02:30:00 +0800

    set a msg if there are no customer in the database

--- app.js
+++ app.js
@@ -4,20 +4,6 @@ const mongoose = require("mongoose");
 const Customers = require("./src/models/Customers");
 const prompt = require("prompt-sync")();
 
-// const username = prompt("What is your name? ");
-// console.log(`Your name is ${username}`);
-
-// const crmMenu = prompt(`Welcome to the CRM \n
-// What would you like to do?\n
-//     1. Create a customer
-//     2. View all customers
-//     3. Upodate a customer
-//     4. Delete a customer
-//     5. quit\n`);
-
-// console.log(`Number of action to run: \n
-//     # user inputs ${crmMenu}`);
-
 const crmMenu = async () => {
   const menu = prompt(`Welcome to the CRM \n
         What would you like to do?\n
@@ -52,62 +38,24 @@ const crmMenu = async () => {
 };
 /*------------------------------ Starter Code ------------------------------*/
 
-// const mongoose = require("mongoose");
-// const Todo = require("./models/todo.js");
-
-// const connect = async () => {
-//   await mongoose.connect(process.env.MONGODB_URI);
-//   console.log("Connected to MongoDB");
-//   await runQueries();
-
-//   await mongoose.disconnect();
-//   console.log("Disconnected from MongoDB");
-//   process.exit();
-// };
-
-// connect();
-
 const connect = async () => {
   await mongoose.connect(process.env.MONGODB_URI);
   console.log("Connected to MongoDB");
   await crmMenu();
-
-  //   await mongoose.disconnect();
-  //   console.log("Disconnected from MongoDB");
-  //   process.exit();
 };
 
 connect();
 /*----------------------------- Query Functions -----------------------------*/
 
-// const createTodo = async () => {
-//   const todoData = {
-//     text: "learn React",
-//     isComplete: false,
-//   };
-//   const todo = await Todo.create(todoData);
-//   console.log("New todo:", todo);
-// };
-
-// const findTodos = async () => {
-//   const todos = await Todo.find({});
-//   console.log("All todos:", todos);
-// };
-
-// const createCustomer = async () => {
-//   const customerData = {
-//     name: "Alvin",
-//     age: 20,
-//     isComplete: false,
-//   };
-//   const customer = await customersModel.create(customerData);
-//   console.log("New customer:", customer);
-// };
-
 const viewAllCustomers = async () => {
   const allCustomers = await Customers.find();
-  console.log("All customer:", allCustomers);
-  crmMenu();
+  if (!allCustomers.length === 0) {
+    console.log("All customer:", allCustomers);
+    crmMenu();
+  } else {
+    console.log("There are no customers. Please add some to the CRM");
+    crmMenu();
+  }
 };
 
 const createCustomer = async () => {
@@ -128,10 +76,4 @@ const deleteCustomer = async () => {
   const customer = await Customers.findByIdAndDelete(customerId);
   console.log("Deleted customer:", customer);
   crmMenu();
-};
-/*------------------------------- Run Queries -------------------------------*/
-
-const runQueries = async () => {
-  console.log("Queries running.");
-  await createTodo();
 };
